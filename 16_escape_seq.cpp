0ed4b07eb5a5b2737409b5fb847d6d7076b9385f
sanketbhujade <86920747+sanketbhujade@users.noreply.github.com>
2022-09-22 17:54:36 +0530

    backslash

---
+++ 16_escape_seq.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"i am double quote \" "<<endl; //here \ is for printng "
+    
+    //another practical use is for printing paths of file explorer
+    
+    cout<<"c:\\program files\\google";
+    
+    return 0;
+}
