33d9e6407ebcd27db8161a08a767a88d18b6d59e
sanketbhujade <86920747+sanketbhujade@users.noreply.github.com>
2022-09-22 17:30:13 +0530

    precision

---
+++ 15_setting_precision.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a=1.0123456789987654;
+    float b=1.78945612300123456;
+    cout<<"prev a is ="<<a<<endl;
+    cout<<"prev b is ="<<b<<endl;
+    cout<<setprecision(10);
+    cout<<"a is ="<<a<<endl;
+    cout<<"b is ="<<b<<endl;
+    
+    return 0;
+}
